92c2587ddf96b14dc04efb12a86f42afef10a9f1
GRafikjanov <gayratrafikjanov@gmail.com>
2025-01-10 20:22:32 +0500

    created 'Task' model in taks/models.py , created 'objects' inside Task model, added parametres and constraints

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_completed', 'created_at')     # Поля для отображения в списке
+    search_fields = ('title', 'description')    # Возможность поиска по этим полям
+    list_filter = ('is_completed',)    # Фильтрация по статусу завершенности
+
+
+admin.site.register(Task, TaskAdmin)
