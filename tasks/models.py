c6ae6b495589d0334a2ead790d7db7350ee3f959
GRafikjanov <gayratrafikjanov@gmail.com>
2025-01-24 12:29:03 +0500

    created Task model in tasks/models.py

--- tasks/models.py
+++ tasks/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Task(models.Model):
-  title = models.CharField(max_length=255, blank=False, unique=True, null=True)
-  description = models.TextField(max_length=2000, blank=False, null=True)
-  is_completed = models.BooleanField(default=False)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=20, blank=False, unique=True, null=True)
+    description = models.TextField(max_length=20, blank=False, null=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-  def __str__(self):
-    return self.title
-
-
+    def __str__(self):
+        return self.title
